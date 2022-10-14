b7f446f6aba7f8b7d3c7f7a4305aca04d0e2d9bb
Shane Banks <shane.banks02@gmail.com>
2022-10-14 14:38:53 -0500

    Initial setup

---
+++ src/test/java/com/example/pollingapp/PollingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pollingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
