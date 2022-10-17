935ee3329ce063224c938d8dc9469886bd69e4a8
Shane Banks <shane.banks02@gmail.com>
2022-10-17 12:42:58 -0500

    customuserdetailsservice complete

--- src/main/java/com/example/polls/security/CustomUserDetailsService.java
+++ src/main/java/com/example/polls/security/CustomUserDetailsService.java
@@ -1,5 +1,34 @@
 package com.example.polls.security;
 
-public class CustomUserDetailsService {
+import com.example.polls.model.User;
+import com.example.polls.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+	@Autowired
+	UserRepository userRepository;
+
+	@Override
+	@Transactional
+	public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
+		User user = userRepository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail)
+				.orElseThrow(() ->
+						new UsernameNotFoundException("User not found with isername or email : " + usernameOrEmail)
+				);
+		return UserPrincipal.create(user);
+	}
+
+	@Transactional
+	public UserDetails loadUserById(Long id) {
+		User user = userRepository.findById(id).orElseThrow(
+				() -> new UsernameNotFoundException("User not found with ID : " + id)
+		);
+		return UserPrincipal.create(user);
+	}
 }
