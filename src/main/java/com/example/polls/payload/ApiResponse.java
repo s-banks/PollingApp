d9686295f39f8a7dd7e07d7e2847565196d87491
Shane Banks <shane.banks02@gmail.com>
2022-10-17 15:15:14 -0500

    authcontroller done and apiresponse name corrected

--- src/main/java/com/example/polls/payload/ApiResponse.java
+++ src/main/java/com/example/polls/payload/ApiResponse.java
@@ -1,11 +1,11 @@
 package com.example.polls.payload;
 
-public class ApiResponce {
+public class ApiResponse {
 	private Boolean success;
 
 	private String message;
 
-	public ApiResponce(Boolean success, String message) {
+	public ApiResponse(Boolean success, String message) {
 		this.success = success;
 		this.message = message;
 	}
