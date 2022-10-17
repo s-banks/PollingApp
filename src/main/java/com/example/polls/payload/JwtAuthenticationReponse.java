5cbd535d11c2ce2a221676e0fc629d77cd3d63ef
Shane Banks <shane.banks02@gmail.com>
2022-10-17 14:43:08 -0500

    jwtauthenticationresponse complete

--- src/main/java/com/example/polls/payload/JwtAuthenticationReponse.java
+++ src/main/java/com/example/polls/payload/JwtAuthenticationReponse.java
@@ -1,4 +1,27 @@
 package com.example.polls.payload;
 
 public class JwtAuthenticationReponse {
+	private String accessToken;
+
+	private String tokenType = "Bearer";
+
+	public JwtAuthenticationReponse(String accessToken) {
+		this.accessToken = accessToken;
+	}
+
+	public String getAccessToken() {
+		return accessToken;
+	}
+
+	public void setAccessToken(String accessToken) {
+		this.accessToken = accessToken;
+	}
+
+	public String getTokenType() {
+		return tokenType;
+	}
+
+	public void setTokenType(String tokenType) {
+		this.tokenType = tokenType;
+	}
 }
