aee984a468321998c5400bddf3273732a7dcda6a
Shane Banks <shane.banks02@gmail.com>
2022-10-17 15:24:51 -0500

    typo correction in securityconfig plus example properties update

--- src/main/java/com/example/polls/config/SecurityConfig.java
+++ src/main/java/com/example/polls/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -44,7 +45,7 @@ CustomUserDetailsService customUserDetailsService;
 				.passwordEncoder(passwordEncoder());
 	}
 
-@Bean(BeansIds.AUTHENTICATION_MANAGER)
+@Bean(BeanIds.AUTHENTICATION_MANAGER)
 @Override
 	public AuthenticationManager authenticationManagerBean() throws Exception {
 	return super.authenticationManagerBean();
