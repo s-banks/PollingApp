2e2c995ec65faa0114ed61221a012864322bb716
Shane Banks <shane.banks02@gmail.com>
2022-10-17 14:44:43 -0500

    apiresponse complete

--- src/main/java/com/example/polls/payload/ApiResponce.java
+++ src/main/java/com/example/polls/payload/ApiResponce.java
@@ -1,4 +1,28 @@
 package com.example.polls.payload;
 
 public class ApiResponce {
+	private Boolean success;
+
+	private String message;
+
+	public ApiResponce(Boolean success, String message) {
+		this.success = success;
+		this.message = message;
+	}
+
+	public Boolean getSuccess() {
+		return success;
+	}
+
+	public void setSuccess(Boolean success) {
+		this.success = success;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
 }
